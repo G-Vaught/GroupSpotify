5759d8f5b3034144672c33aecf8c4cb956c4554d
Gabriel Vaught <gabeavaught@gmail.com>
2022-08-06 12:12:34 -0500

    Pad date

--- server/server.js
+++ server/server.js
@@ -496,7 +496,7 @@ const updateGroup = async group => {
     await spotifyApi.addTracksToPlaylist(group.spotifyPlaylistID, group.currentTracks.map(track => "spotify:track:" + track));
     let date = new Date().toLocaleString("en-US", { timeZone: "America/Chicago" });
     date = new Date(date);
-    const formattedDate = (date.getMonth() + 1).toString().padStart(2, "0") + "/" + date.getDate();
+    const formattedDate = (date.getMonth() + 1).toString().padStart(2, "0") + "/" + date.getDate().toString().padStart(2, "0");
     await spotifyApi.changePlaylistDetails(group.spotifyPlaylistID, { name: group.name + " - " + formattedDate, description: "Users in the group: " + group.users.map(user => user.user.name) });
 }
 
